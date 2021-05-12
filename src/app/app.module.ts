fdfb5170e5367fb3d17eeeedd9ef150f290d2f7c
Cristian Parra <cristian.a.parra@gmail.com>
2021-05-12 19:21:59 -0400

    creando modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/heroe/heroe.component';
-import { AppContador } from './contador/contador.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+
+import { HeroesModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppContador,
-    HeroComponent,
-    ListadoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { AppContador } from './contador/contador.component';
+
+@NgModule({
+declarations:[
+  AppContador
+],
+exports:[
+  AppContador
+],
+imports:[
+
+]
+})
+
+export class contadorModule { }
